51db1782865b91c9331906a7f6e0acfb44cfdb2f
Sneider <electronsgm@gmail.com>
2022-12-26 13:27:56 -0500

    FEAT(react-folder): Add a basic functional component to test in the store

---
+++ react/BulletGroup.tsx
@@ -0,0 +1,3 @@
+import BulletGroup from "./components/BulletGroup";
+
+export default BulletGroup;
---
+++ react/components/BulletGroup.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const BulletGroup = () => {
+  return (
+    <div>Here is a bullet group</div>
+  )
+}
+
+export default BulletGroup;
